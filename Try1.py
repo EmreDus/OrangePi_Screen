9a4fb1ff8af13c56048005a783f44723f9d20a25
Muhammet Emre DUŞ <emredus@gmail.com>
2023-03-07 00:45:49 +0300

    Update Try1.py

--- Try1.py
+++ Try1.py
@@ -7,7 +7,13 @@ from pyA20.gpio import gpio
 from pyA20.gpio import port
 from pyA20.gpio import connector
 
+# Set up the pins as outputs
 gpio.init()
+gpio.setcfg(port.PA14, gpio.OUTPUT)  # Clock
+gpio.setcfg(port.PA15, gpio.OUTPUT)  # MOSI
+gpio.setcfg(port.PA13, gpio.OUTPUT)  # CS
+gpio.setcfg(port.PA1, gpio.OUTPUT)   # DC
+gpio.setcfg(port.PA0, gpio.OUTPUT)   # Reset
 
 spi = busio.SPI(clock=port.PA14, MOSI=port.PA15)
 
@@ -17,7 +23,10 @@ reset_pin = port.PA0
 
 display = st7735.ST7735R(spi, cs=cs_pin, dc=dc_pin, rst=reset_pin, width=128, height=128, colstart=2, rowstart=1)
 
+# Draw a rectangle and some text on the display
 display.fill(st7735.BLACK)
-display.pixel(10, 10, st7735.WHITE)
+display.rectangle(10, 10, 30, 30, st7735.WHITE)
 display.text("Hello, world!", 0, 0, st7735.WHITE)
+
+# Update the display
 display.show()
