e5ec7800bf4b01cd8f6965023d233a5c22526574
Muhammet Emre DUŞ <emredus@gmail.com>
2023-03-06 22:38:44 +0300

    Create Try1.py

---
+++ Try1.py
@@ -0,0 +1,21 @@
+import busio
+import adafruit_st7735r as st7735
+
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+from pyA20.gpio import connector
+
+gpio.init()
+
+spi = busio.SPI(clock=port.PA14, MOSI=port.PA15, MISO=port.PA16)
+
+cs_pin = port.PA13
+dc_pin = port.PA1
+reset_pin = port.PA0
+
+display = st7735.ST7735R(spi, cs=cs_pin, dc=dc_pin, rst=reset_pin, width=128, height=128, colstart=2, rowstart=1)
+
+display.fill(st7735.BLACK)
+display.pixel(10, 10, st7735.WHITE)
+display.text("Hello, world!", 0, 0, st7735.WHITE)
+display.show()
